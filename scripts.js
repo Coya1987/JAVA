dee8c2e87a4c7d2348bb6941b892e7bad1272ecb
Coya1987 <jemartinez1987@gmail.com>
2022-07-28 14:13:03 -0300

    primer comit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let planeta = prompt("planeta mas cercano al sol");
+if (planeta == "mercurio"){
+    console.log("perfecto");
+}else{
+    console.log("no es correcto");
+}
+
+let edad = parseInt(prompt("cual es tu edad?"));
+if (edad => 18){
+    console.log("bienvenido");
+}else (edad < 18){
+    console.log("eres menor");
+}
