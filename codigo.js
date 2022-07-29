d5e1412a30abd9f621d10d3f8e2844fa5f216181
Coya1987 <jemartinez1987@gmail.com>
2022-07-29 17:34:03 -0300

    agregandp js a mi proyecto

---
+++ codigo.js
@@ -0,0 +1,9 @@
+let total = 0;
+let producto1 = 14500;
+let producto2 = 12400;
+
+function sumar(producto1,porducto2){
+    total = producto1 + producto2
+    console.log("la suma de $ "+producto1+" + "+porducto2+" es igual $"+ total);
+}
+sumar(producto1,producto2);
