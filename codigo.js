c71efb2944dc733da561c3fbeac650bfb6ac4c91
Coya1987 <jemartinez1987@gmail.com>
2022-08-04 14:06:21 -0300

    agregar array a mi proyecto

--- codigo.js
+++ codigo.js
@@ -1,14 +1,15 @@
-
-// for(intentos=1;intentos<=3;intentos++){
-//     let contrasenia=prompt("Ingrese la Contraseña");
-//     if(contrasenia==1987){
-//         console.log("Bienvenido a la Tienda River");
-//         break;
-//     }else{
-//         console.log("La contraseña es Incorrecta")
+// function ingresePassword(){
+//     for(intentos=1;intentos<=3;intentos++){
+//         let contrasenia=prompt("Ingrese la Contraseña");
+//         if(contrasenia==1987){
+//             console.log("Bienvenido a la Tienda River");
+//             break;
+//         }else{
+//             console.log("La contraseña es Incorrecta")
+//         }
 //     }
 // }
-
+// ingresePassword();
 
 // let total = 0;
 // //let producto1 = 14500;
@@ -47,5 +48,14 @@ const producto1 = new producto ("Camiseta Titular", 2022, 14500, true);
 const producto2 = new producto ("Camiseta Alternativa", 2022, 12400, true);
 const producto3 = new producto ("Camiseta Arquero", 2022, 11000, true);
 const producto4 = new producto ("Camiseta Titular", 2021, 14500, false);
-console.log(producto1)
-producto3.mostrarProducto();
+const producto5 = new producto ("Musculosa", 2020, 8500, true);
+const producto6 = new producto ("Chomba", 2021, 7800, false);
+const producto7 = new producto ("Conjunto Invierno", 2021, 18900, true);
+
+
+const  productos = []
+productos.push(producto1, producto2, producto3, producto4, producto5, producto6, producto7);
+console.table(productos);
+
+console.log(producto7);
+producto7.mostrarProducto();
