865b7a5c39cb9eb9aef1e8db372c18ed1ac6c67e
Coya1987 <jemartinez1987@gmail.com>
2022-09-23 16:46:45 -0300

    hacemos funcionar el carrito correctamente, agregamos estilo a botones y ordenamos codigo

--- codigo.js
+++ codigo.js
@@ -1,108 +1,10 @@
-// function ingresePassword(){
-//     for(intentos=1;intentos<=3;intentos++){
-//         let contrasenia=prompt("Ingrese la Contraseña");
-//         if(contrasenia==1987){
-//             console.log("Bienvenido a la Tienda River");
-//             break;
-//         }else{
-//             console.log("La contraseña es Incorrecta")
-//         }
-//     }
-// }
-// ingresePassword();
 
-// let total = 0;
-// let camiseta1 = 14500;
-// let camiseta2 = 12400;
-
-// function comprar(camiseta1,camiseta2){
-//     total = camiseta1 + camiseta2
-//     console.log("la suma de $ "+camiseta1+" + $"+camiseta2+" es igual $"+ total);
-    
-// }
-// comprar(camiseta1,camiseta2);
-
-// function comprarConCuotas(camiseta1){
-//     totalConCuotas= camiseta1/12;
-//     console.log("La camiseta Titular de River Plate te queda en 12 cuotas de $"+totalConCuotas);
-// }
-// comprarConCuotas(camiseta1);
-
-
-// // //array tienda
-
-// class producto{
-//     constructor (nombre, anio, precio, disponibilidad){
-//         this.nombre = nombre;
-//         this.anio = anio;
-//         this.precio = precio;
-//         this.disponibilidad = disponibilidad;
-//     }
-//     mostrarProducto(){
-//         console.log("nombre: "+this.nombre+"\nAño: "+this.anio+"\nPrecio: "+this.precio+"\nDisponibilidad "+this.disponibilidad);
-//     }
-// }
-// const producto1 = new producto ("Camiseta Titular", 2022, 14500, true);
-// const producto2 = new producto ("Camiseta Alternativa", 2022, 12400, true);
-// const producto3 = new producto ("Camiseta Arquero", 2022, 11000, true);
-// const producto4 = new producto ("Camiseta Titular", 2021, 14500, false);
-// const producto5 = new producto ("Musculosa", 2020, 8500, true);
-// const producto6 = new producto ("Chomba", 2021, 7800, false);
-// const producto7 = new producto ("Conjunto Invierno", 2021, 18900, true);
-
-
-// const  productos = []
-// productos.push(producto1, producto2, producto3, producto4, producto5, producto6, producto7);
-// console.table(productos);
-
-// console.log(producto7);
-// producto7.mostrarProducto();
-
-// //filter
-
-// let disponibles = productos.filter(producto=> producto.disponibilidad==true);
-// console.table(disponibles);
-
-// let noDisponibles = productos.filter(producto=> producto.disponibilidad==false);
-// console.table(noDisponibles);
-
-// //find
-
-// let porNombre = productos.find(producto=>producto.nombre=="Conjunto Invierno");
-// console.log(porNombre);
-
-// // uasuario agrega nuevo producto
-
-// let nombreNuevo = prompt("Ingrese el Nombre del Producto Nuevo");
-// let anioNuevo = Number(prompt("ingrese el Año del Producto Nuevo"));
-// let precioNuevo = Number(prompt("Ingrese el Precio del Producto Nuevo"));
-// let disponibilidadNuevo = confirm("El Nuevo Producto esta Disponible?");
-
-// let productoNuevo = new producto(nombreNuevo, anioNuevo, precioNuevo, disponibilidadNuevo);
-
-// productos.push(productoNuevo);
-
-// console.table(productos);
-
-// // array + iva con map
-
-// const productoIva = productos.map(producto =>{
-//     return{
-//         nombre:producto.nombre,
-//         anio:producto.anio,
-//         precio:producto.precio * 1.21,
-//         disponibilidad:producto.disponibilidad
-//     }
-// });
-
-// console.table(productoIva);
-
-// DOM
-localStorage.clear();
+localStorage.clear();    //LIMPIAMOS EL LOCAL-STORAGE PARA EVITAR ACUMULACION DE DATOS INNECESARIOS (SE PUEDE DESACTIVAR SOLO COMENTANDO LA LINEA)
 let lista=document.getElementById("contenedor-pagina");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let productosJSON = [];
 
+// DOM
 function crearProductos(){
     for(const producto of productosJSON) {
         lista.innerHTML+=(`<div class="contenedor-productos">
@@ -119,7 +21,6 @@ function crearProductos(){
     });
 }
 
-crearProductos();
 
 function agregarAlCarrito(productoNuevo){
     let encontrado = carrito.find(p => p.id == productoNuevo.id);
@@ -141,12 +42,11 @@ function agregarAlCarrito(productoNuevo){
         });
         document.getElementById("items").innerHTML+=(`
         <tr>
-        <td id='fila ${prodACarrito.id}'>${prodACarrito.id} </td>
+        <td id='fila${prodACarrito.id}'>${prodACarrito.id} </td>
         <td>${prodACarrito.nombre}</td>
         <td id='${prodACarrito.id}'> ${prodACarrito.cantidad}</td>
         <td>$${prodACarrito.precio}</td>
-        <td>$${prodACarrito.precio*prodACarrito.cantidad}</td>
-        <td><button onclick="borrar(${prodACarrito.id})" class="delete" scope="row"><i class="bi bi-trash"></i></button></td>
+        <td><button onclick="eliminar(${prodACarrito.id})" class="delete" scope="row"><i class="bi bi-trash"></i></button></td>
         </tr> 
         `);
         
@@ -166,7 +66,7 @@ function agregarAlCarrito(productoNuevo){
     document.getElementById("gastoTotal").innerText=(`Total: $ ${calcularTotal()}`);
     localStorage.setItem("carrito",JSON.stringify(carrito));
 }
-
+// SUMAMOS EL PRECIO DE LOS PRODUCTOS POR LA CANTIDAD QUE QUEREMOS COMPRAR DE CADA UNO
 function calcularTotal () {
     let suma = 0;
     for (const prodACarrito of carrito) {
@@ -174,18 +74,19 @@ function calcularTotal () {
     }
     return suma;
 }
-
-function borrar(id){
+// FUCION PARA BORRAR LOS PRODUCTOS DEL CARRO
+function eliminar(id){
     let indice = carrito.findIndex(producto => producto.id==id);
     carrito.splice(indice,1);
-    let fila = document.getElementById(`fila${id}`);
-    document.getElementById("tabla").removeChild(fila);
-    document.getElementById("gastoTotal").innerText(`Total: $ ${calcularTotal()}`);
+    // console.log();
+    let fila = document.getElementById(`fila${id}`).parentElement;
+    document.getElementById("items").removeChild(fila);
+    document.getElementById("gastoTotal").textContent = `Total: $ ${calcularTotal()}`;
     localStorage.setItem("carrito",JSON.stringify(carrito));
-    
+    Swal.fire("Producto Eliminado");
+    if(carrito.length == 0) document.getElementById("gastoTotal").innerHTML = `<th scope="row"  colspan="5">--Carrito vacío--</th>`;
 }
-
-
+// traemos el JSON
 async function traerJSON() {
     const URLJSON="productos.json"
     const respuesta=await fetch(URLJSON)
